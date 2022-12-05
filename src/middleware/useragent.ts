107cae749bda8037f54d580255ac2a5578597b90
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-12-05 16:44:09 +0000

    0.2.4

--- src/middleware/useragent.ts
+++ src/middleware/useragent.ts
@@ -8,7 +8,7 @@ export class UserAgent {
                 ...context.init,
                 headers: {
                     ...context.init.headers,
-                    "User-Agent": "falconjs/0.2.3",
+                    "User-Agent": "falconjs/0.2.4",
                 },
             },
         };
