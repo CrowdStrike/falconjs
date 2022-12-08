e2c05b200591b61007bd2bea173fa9c9c25683f8
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-12-08 15:13:24 +0000

    Add missing endpoints to the high-level API: FalconClient

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,7 @@ import { EventStream } from "./event_stream";
 import { FalconCloud } from "./FalconCloud";
 import { OAuth2, UserAgent } from "./middleware";
 import {
+    AlertsApi,
     CloudConnectAwsApi,
     CspmRegistrationApi,
     CustomIoaApi,
@@ -12,6 +13,7 @@ import {
     EventStreamsApi,
     FalconCompleteDashboardApi,
     FalconContainerApi,
+    FalconContainerCliApi,
     FalconxSandboxApi,
     FilevantageApi,
     FirewallManagementApi,
@@ -21,6 +23,7 @@ import {
     IdentityProtectionApi,
     IncidentsApi,
     InstallationTokensApi,
+    InstallationTokensSettingsApi,
     IntelApi,
     IoaExclusionsApi,
     IocApi,
@@ -29,8 +32,10 @@ import {
     MalqueryApi,
     MessageCenterApi,
     MlExclusionsApi,
+    MobileEnrollmentApi,
     MsspApi,
     Oauth2Api,
+    OdsApi,
     OverwatchDashboardApi,
     PreventionPoliciesApi,
     QuarantineApi,
@@ -45,10 +50,13 @@ import {
     SensorDownloadApi,
     SensorUpdatePoliciesApi,
     SensorVisibilityExclusionsApi,
+    SpotlightEvaluationLogicApi,
     UserManagementApi,
     ZeroTrustAssessmentApi,
 } from "./apis";
 import { SpotlightVulnerabilitiesApi } from "./apis/SpotlightVulnerabilitiesApi";
+import { D4cRegistrationApi } from "./apis/D4cRegistrationApi";
+import { TailoredIntelligenceApi } from "./apis/TailoredIntelligenceApi";
 
 export interface FalconClientOptions {
     /** override for fetch implementation (on node use node-fetch or cross-fetch) */
@@ -69,15 +77,18 @@ export interface FalconClientOptions {
 }
 
 export class FalconClient {
+    alerts: AlertsApi;
     cloudConnectAws: CloudConnectAwsApi;
     cspmRegistration: CspmRegistrationApi;
     customIoa: CustomIoaApi;
+    d4cRegistrationApi: D4cRegistrationApi;
     detects: DetectsApi;
     deviceControlPolicies: DeviceControlPoliciesApi;
     discover: DiscoverApi;
     eventStreams: EventStreamsApi;
     falconCompleteDashboard: FalconCompleteDashboardApi;
     falconContainer: FalconContainerApi;
+    falconContainerCli: FalconContainerCliApi;
     falconxSandbox: FalconxSandboxApi;
     filevantage: FilevantageApi;
     firewallManagement: FirewallManagementApi;
@@ -87,6 +98,7 @@ export class FalconClient {
     identityProtection: IdentityProtectionApi;
     incidents: IncidentsApi;
     installationTokens: InstallationTokensApi;
+    installationTokensSettings: InstallationTokensSettingsApi;
     intel: IntelApi;
     ioaExclusions: IoaExclusionsApi;
     ioc: IocApi;
@@ -95,8 +107,10 @@ export class FalconClient {
     malquery: MalqueryApi;
     messageCenter: MessageCenterApi;
     mlExclusions: MlExclusionsApi;
+    mobileEnrollment: MobileEnrollmentApi;
     mssp: MsspApi;
     oauth2: Oauth2Api;
+    ods: OdsApi;
     overwatchDashboard: OverwatchDashboardApi;
     preventionPolicies: PreventionPoliciesApi;
     quarantine: QuarantineApi;
@@ -111,7 +125,9 @@ export class FalconClient {
     sensorDownload: SensorDownloadApi;
     sensorUpdatePolicies: SensorUpdatePoliciesApi;
     sensorVisibilityExclusions: SensorVisibilityExclusionsApi;
+    spotlightEvaluationLogic: SpotlightEvaluationLogicApi;
     spotlightVulnerabilities: SpotlightVulnerabilitiesApi;
+    tailoredIntelligenceapi: TailoredIntelligenceApi;
     userManagement: UserManagementApi;
     zeroTrustAssessment: ZeroTrustAssessmentApi;
     private config: Configuration;
@@ -129,15 +145,18 @@ export class FalconClient {
             accessToken: oauth2.accessToken.bind(oauth2),
             middleware: [new UserAgent()],
         });
+        this.alerts = new AlertsApi(this.config);
         this.cloudConnectAws = new CloudConnectAwsApi(this.config);
         this.cspmRegistration = new CspmRegistrationApi(this.config);
         this.customIoa = new CustomIoaApi(this.config);
+        this.d4cRegistrationApi = new D4cRegistrationApi(this.config);
         this.detects = new DetectsApi(this.config);
         this.deviceControlPolicies = new DeviceControlPoliciesApi(this.config);
         this.discover = new DiscoverApi(this.config);
         this.eventStreams = new EventStreamsApi(this.config);
         this.falconCompleteDashboard = new FalconCompleteDashboardApi(this.config);
         this.falconContainer = new FalconContainerApi(this.config);
+        this.falconContainerCli = new FalconContainerCliApi(this.config);
         this.falconxSandbox = new FalconxSandboxApi(this.config);
         this.filevantage = new FilevantageApi(this.config);
         this.firewallManagement = new FirewallManagementApi(this.config);
@@ -147,6 +166,7 @@ export class FalconClient {
         this.identityProtection = new IdentityProtectionApi(this.config);
         this.incidents = new IncidentsApi(this.config);
         this.installationTokens = new InstallationTokensApi(this.config);
+        this.installationTokensSettings = new InstallationTokensSettingsApi(this.config);
         this.intel = new IntelApi(this.config);
         this.ioaExclusions = new IoaExclusionsApi(this.config);
         this.ioc = new IocApi(this.config);
@@ -155,8 +175,10 @@ export class FalconClient {
         this.malquery = new MalqueryApi(this.config);
         this.messageCenter = new MessageCenterApi(this.config);
         this.mlExclusions = new MlExclusionsApi(this.config);
+        this.mobileEnrollment = new MobileEnrollmentApi(this.config);
         this.mssp = new MsspApi(this.config);
         this.oauth2 = new Oauth2Api(this.config);
+        this.ods = new OdsApi(this.config);
         this.overwatchDashboard = new OverwatchDashboardApi(this.config);
         this.preventionPolicies = new PreventionPoliciesApi(this.config);
         this.quarantine = new QuarantineApi(this.config);
@@ -171,7 +193,9 @@ export class FalconClient {
         this.sensorDownload = new SensorDownloadApi(this.config);
         this.sensorUpdatePolicies = new SensorUpdatePoliciesApi(this.config);
         this.sensorVisibilityExclusions = new SensorVisibilityExclusionsApi(this.config);
+        this.spotlightEvaluationLogic = new SpotlightEvaluationLogicApi(this.config);
         this.spotlightVulnerabilities = new SpotlightVulnerabilitiesApi(this.config);
+        this.tailoredIntelligenceapi = new TailoredIntelligenceApi(this.config);
         this.userManagement = new UserManagementApi(this.config);
         this.zeroTrustAssessment = new ZeroTrustAssessmentApi(this.config);
     }
