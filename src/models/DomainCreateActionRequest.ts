34d49cee3b3239d69f409c1c0553820614fcd968
Simon Lukasik <simon.lukasik@crowdstrike.com>
2023-02-23 09:27:04 +0000

    Re-generate the codebase using the latest swagger

--- src/models/DomainCreateActionRequest.ts
+++ src/models/DomainCreateActionRequest.ts
@@ -20,31 +20,31 @@ import { exists, mapValues } from "../runtime";
  */
 export interface DomainCreateActionRequest {
     /**
-     *
+     * The level of detail in which the content will be delivered. It can be either 'standard' or 'enhanced'
      * @type {string}
      * @memberof DomainCreateActionRequest
      */
     contentFormat: string;
     /**
-     *
+     * The time interval between the action's triggers. It can be one of the values: 'asap', 'daily' or 'weekly'
      * @type {string}
      * @memberof DomainCreateActionRequest
      */
     frequency: string;
     /**
-     *
+     * The address list who will be notified by this action.
      * @type {Array<string>}
      * @memberof DomainCreateActionRequest
      */
     recipients: Array<string>;
     /**
-     *
+     * Whether to periodically trigger the action based on the frequency, even when there are no new matches for the associated monitoring rule
      * @type {boolean}
      * @memberof DomainCreateActionRequest
      */
     triggerMatchless: boolean;
     /**
-     *
+     * The action type. The only type currently supported is 'email'
      * @type {string}
      * @memberof DomainCreateActionRequest
      */
