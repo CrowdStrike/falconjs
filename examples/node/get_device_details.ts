82be3ee352b0c2a7a290566900425715761e3dee
Clayton Burlison <git@clburlison.com>
2023-12-04 11:01:44 -0600

    feat: Add additional sample files

---
+++ examples/node/get_device_details.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+import fetch from "cross-fetch";
+import { FalconClient } from "../../src";
+
+function falconClientFromEnv() {
+    if (!process.env.FALCON_CLIENT_ID) {
+        throw new Error(
+            "Missing FALCON_CLIENT_ID environment variable. Please provide your OAuth2 API Client ID for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys."
+        );
+    }
+    if (!process.env.FALCON_CLIENT_SECRET) {
+        throw new Error(
+            "Missing FALCON_CLIENT_SECRET environment variable. Please provide your OAuth2 API Client Secret for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys."
+        );
+    }
+    const cloud = process.env.FALCON_CLOUD;
+    if (!cloud) {
+        throw new Error("Missing FALCON_CLOUD environment variable.");
+    }
+    if (cloud != "us-1" && cloud != "us-2" && cloud != "eu-1" && cloud != "us-gov-1") {
+        throw new Error("Invalid FALCON_CLOUD environment variable: " + cloud);
+    }
+    if (!process.env.FALCON_DEVICE_HOST_ID) {
+        throw new Error("Missing FALCON_DEVICE_HOST_ID environment variable. Please provide a host ID to fetch device details for.");
+    }
+    return new FalconClient({
+        fetchApi: fetch,
+        cloud: cloud,
+        clientId: process.env.FALCON_CLIENT_ID,
+        clientSecret: process.env.FALCON_CLIENT_SECRET,
+    });
+}
+
+const client = falconClientFromEnv();
+const response = client.hosts
+    .getDeviceDetailsV2([process.env.FALCON_DEVICE_HOST_ID!])
+    .then((value) => {
+        console.log("Device details: ", value);
+    })
+    .catch((err) => {
+        console.log("Could not fetch device: ", err);
+    });
+
+console.log(response);
---
+++ examples/node/rtr.ts
@@ -0,0 +1,86 @@
+#!/usr/bin/node
+
+import fetch from "cross-fetch";
+import { FalconClient, FalconErrorExplain, FalconClientOptions } from "../../src";
+import { run } from "node:test";
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function falconClientFromEnv() {
+    if (!process.env.FALCON_CLIENT_ID) {
+        throw new Error(
+            "Missing FALCON_CLIENT_ID environment variable. Please provide your OAuth2 API Client ID for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys."
+        );
+    }
+    if (!process.env.FALCON_CLIENT_SECRET) {
+        throw new Error(
+            "Missing FALCON_CLIENT_SECRET environment variable. Please provide your OAuth2 API Client Secret for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys."
+        );
+    }
+    const cloud = process.env.FALCON_CLOUD;
+    if (!cloud) {
+        throw new Error("Missing FALCON_CLOUD environment variable.");
+    }
+    if (cloud != "us-1" && cloud != "us-2" && cloud != "eu-1" && cloud != "us-gov-1") {
+        throw new Error("Invalid FALCON_CLOUD environment variable: " + cloud);
+    }
+    if (!process.env.FALCON_DEVICE_HOST_ID) {
+        throw new Error("Missing FALCON_DEVICE_HOST_ID environment variable. Please provide a host ID to fetch device details for.");
+    }
+    let clientConfig: FalconClientOptions = {
+        fetchApi: fetch,
+        cloud: cloud,
+        clientId: process.env.FALCON_CLIENT_ID,
+        clientSecret: process.env.FALCON_CLIENT_SECRET,
+    };
+
+    if (typeof process.env.FALCON_MEMBER_CID === "string") {
+        clientConfig.memberCid = process.env.FALCON_MEMBER_CID;
+    }
+
+    return new FalconClient(clientConfig);
+}
+
+const client = falconClientFromEnv();
+
+try {
+    const startSession = await client.realTimeResponse.rTRInitSession({ deviceId: process.env.FALCON_DEVICE_HOST_ID!, origin: "", queueOffline: true }, 300);
+    console.log("Session ID: ", startSession.resources[0].sessionId);
+
+    const runLsCmd = await client.realTimeResponseAdmin.rTRExecuteAdminCommand({
+        baseCommand: "ls",
+        commandString: "ls /Users",
+        id: 0,
+        deviceId: process.env.FALCON_DEVICE_HOST_ID!,
+        persist: true,
+        sessionId: startSession.resources[0].sessionId,
+    });
+    const queuedSessionInfo1 = await client.realTimeResponse.rTRListQueuedSessions({ ids: [startSession.resources[0].sessionId] });
+    console.log("Queued Session Info: ", queuedSessionInfo1);
+    console.log("LS Command: ", runLsCmd);
+    await delay(4000);
+    const queuedSessionInfo2 = await client.realTimeResponse.rTRListQueuedSessions({ ids: [startSession.resources[0].sessionId] });
+    console.log("Queued Session Info: ", queuedSessionInfo2);
+
+    const lsCmdResults = await client.realTimeResponseAdmin.rTRCheckAdminCommandStatus(runLsCmd.resources[0].cloudRequestId, 0);
+    console.log("Command Results: ", lsCmdResults);
+
+    const getFileCmd = await client.realTimeResponseAdmin.rTRExecuteAdminCommand({
+        baseCommand: "get",
+        commandString: "get '/System/Library/Fonts/NewYork.ttf'",
+        id: 1,
+        deviceId: process.env.FALCON_DEVICE_HOST_ID!,
+        persist: true,
+        sessionId: startSession.resources[0].sessionId,
+    });
+    console.log("LS Command: ", runLsCmd);
+
+    const getFileResults = await client.realTimeResponseAdmin.rTRCheckAdminCommandStatus(getFileCmd.resources[0].cloudRequestId, 0);
+    console.log("Command Results: ", getFileResults);
+
+    const queuedSessionInfo = await client.realTimeResponse.rTRListQueuedSessions({ ids: [startSession.resources[0].sessionId] });
+    console.log("Queued Session Info: ", queuedSessionInfo);
+} catch (err) {
+    console.log("Batch Session Command Error", err);
+    console.log(await FalconErrorExplain(err));
+}
