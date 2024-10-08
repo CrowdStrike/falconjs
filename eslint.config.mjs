b683f80e2a70b95956df30b3ef61dec19f4e2903
Evan Stoner <evan.stoner@crowdstrike.com>
2024-10-08 14:06:47 -0400

    Remove commented config and prettier the config file.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,27 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  // global ignore (cannot be combined with other config keys)
-  // {
-  //   ignores: ['**/*']
-  // },
-  // default config for this project's typescript
-  {
-    files: ['src/**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["error", {
-        argsIgnorePattern: "^_",
-      }],
+    // default config for this project's typescript
+    {
+        files: ["src/**/*.ts"],
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
+        },
     },
-  },
-  // config for new Falcon uber client
-  {
-    files: ['src/falcon.ts', 'src/endpoints.ts', 'src/_endpoints/*.ts'],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-  }
+    // config for new Falcon uber client
+    {
+        files: ["src/falcon.ts", "src/endpoints.ts", "src/_endpoints/*.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 );
